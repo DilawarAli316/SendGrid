abe8fc8f19d5d35ca7f2aac4996863bee231936f
Sarmed <sarmedrizvi@gmail.com>
2021-01-05 01:03:00 +0500

    updates

--- src/api/v1/restaurants/util/google-apis.js
+++ src/api/v1/restaurants/util/google-apis.js
@@ -4,23 +4,68 @@ const logger = require(appRoot + '/src/logger').apiLogger;
 const constants = require(appRoot + '/src/constant');
 
 exports.getRestaurantByGoogleApi = async (
-  location = '24.8608,67.0104',
-  language = 'en',
+  location = { lat: '24.8608', log: '67.0104' },
+  language = 'fr',
+  pagetoken = '',
 ) => {
   try {
     logger.info(`calling restaurants from google api`);
-    const neighborhood = 'chelsea';
-    const borough = 'manhattan';
-    const city = 'new+york+city';
-    const category = 'burgers';
     const { data } = await axios.get(
-      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?type=restaurant&key=${constants.GOOGLE_API_KEY}&location=${location}&radius=10000`,
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json`,
+      {
+        params: {
+          types: 'bar|restaurant|night_club|cafe',
+          key: constants.GOOGLE_API_KEY,
+          location: `${location.lat},${location.log}`,
+          radius: 10000,
+          language,
+          pagetoken,
+        },
+      },
     );
     logger.info(`succesfully get restaurants`);
-    return data;
+    logger.info(`modifying places according to our needs`);
+    let places = { next_page_token: data.next_page_token };
+    data.results.forEach((place) => {
+      const { geometry: { location: { lat = '', log = '' } = {} } = {} } =
+        place || {};
+      places.results = [
+        {
+          ...place,
+          distance: getDistanceFromLatLonInKm(
+            location.lat,
+            location.log,
+            lat,
+            log,
+          ),
+        },
+        ...(places.results || []),
+      ];
+    });
+
+    return places;
   } catch (error) {
-    logger.error(JSON.stringify((error = error.stack)));
+    logger.error(JSON.stringify(error));
     console.log(error);
     return 'error';
   }
+};
+
+const getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2 - lat1); // deg2rad below
+  var dLon = deg2rad(lon2 - lon1);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c; // Distance in km
+  return d.toFixed(3);
+};
+
+const deg2rad = (deg) => {
+  return deg * (Math.PI / 180);
 };
